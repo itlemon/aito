2780dfec916316db1117a2dcf9a53d92c6fd243b
itlemon <lemon_jiang@aliyun.com>
2024-08-14 00:57:00 +0800

    mod: 调整时间序列化规则

--- aito-model/src/main/java/cn/codingguide/aito/model/response/AitoResponse.java
+++ aito-model/src/main/java/cn/codingguide/aito/model/response/AitoResponse.java
@@ -2,6 +2,7 @@ package cn.codingguide.aito.model.response;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.feiniaojin.gracefulresponse.data.Response;
 import com.feiniaojin.gracefulresponse.data.ResponseStatus;
@@ -27,7 +28,8 @@ public class AitoResponse implements Response {
 
     private Object data;
 
-    private final String timestamp = LocalDateTime.now().toString();
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private final LocalDateTime timestamp = LocalDateTime.now();
 
     @Override
     public void setStatus(ResponseStatus statusLine) {
--- aito-webapp/aito-demo/src/main/java/cn/codingguide/aito/demo/modular/demo/controller/ExampleController.java
+++ aito-webapp/aito-demo/src/main/java/cn/codingguide/aito/demo/modular/demo/controller/ExampleController.java
@@ -5,20 +5,19 @@ import static cn.codingguide.aito.core.constants.ApiConstants.DEMO_API_PREFIX;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author itlemon <lemon_jiang@aliyun.com>
  * Created on 2024-06-18
  */
-@Controller
+@RestController
 @RequestMapping(DEMO_API_PREFIX)
 public class ExampleController {
 
-    @RequestMapping("/hello")
-    @ResponseBody
+    @GetMapping("/hello")
     public Map<String, String> hello() {
         Map<String, String> res = new HashMap<>();
         res.put("Test", "Hello Aito.");
